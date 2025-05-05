eb9b36c233b2e21cfd393c38f4f7a0c1e7865992
Duda Angeli <84750723+DudaAngeli@users.noreply.github.com>
2025-05-05 19:38:26 -0300

    Adição de AcrescimentoMaiorDoQueJurosException.java e NumeroDigitadoInvalidoException.java, dois métodos de tratamento de exceções

---
+++ src/src/util/AcrescimentoMaiorDoQueJurosException.java
@@ -0,0 +1,7 @@
+package src.util;
+
+public class AcrescimentoMaiorDoQueJurosException extends Exception{
+    public AcrescimentoMaiorDoQueJurosException(String errorMessage) {
+        super(errorMessage);
+    }
+}
